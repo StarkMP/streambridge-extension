146f6789f659a342f860076152a691b4d8b32d5e
Maksim Serebryakov <starkmp.work@gmail.com>
2023-06-06 01:32:40 +0300

    Add trovo support

--- src/streaming-platforms/index.ts
+++ src/streaming-platforms/index.ts
@@ -1,4 +1,5 @@
 import kick from './kick';
+import trovo from './trovo';
 import vkPlay from './vkplay';
 
-export default [vkPlay, kick];
+export default [vkPlay, kick, trovo];
---
+++ src/streaming-platforms/trovo.ts
@@ -0,0 +1,59 @@
+import { StreamingPlatform } from '../types';
+import { onElementLoaded } from '../utils/dom';
+
+const trovo: StreamingPlatform = {
+  id: 'trovo.live',
+  getInfo: async (channel) => {
+    const apiUrl = 'https://api-web.trovo.live/graphql?qid=0';
+
+    const body = JSON.stringify([
+      {
+        operationName: 'live_LiveReaderService_GetLiveInfo',
+        variables: {
+          params: {
+            userName: channel.source.channelId,
+            requireDecorations: true,
+          },
+        },
+      },
+    ]);
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+        },
+        cache: 'no-store',
+        body,
+      });
+
+      const json = await response.json();
+      const data = json[0].data.live_LiveReaderService_GetLiveInfo;
+      const isOnline = !!data.isLive;
+
+      return {
+        twitch: channel.twitch,
+        isOnline,
+        category: data.categoryInfo.name,
+        viewers: isOnline ? data.channelInfo.viewers : 0,
+        title: data.channelInfo.title,
+        nickname: data.streamerInfo.nickName,
+        avatar: data.streamerInfo.faceUrl,
+      };
+    } catch (e) {
+      return null;
+    }
+  },
+  render: () => {
+    onElementLoaded('nav.base-nav', (el) => {
+      el.style.display = 'none';
+    });
+
+    onElementLoaded('.base-left-side-container', (el) => {
+      el.style.display = 'none';
+    });
+  },
+};
+
+export default trovo;
