10191ec50f93250d87a1a6fe5344923963024d0c
Maksim Serebryakov <starkmp.work@gmail.com>
2023-06-15 17:56:55 +0300

    Add report a bug btn

--- src/popup/components/Header/index.tsx
+++ src/popup/components/Header/index.tsx
@@ -1,17 +1,25 @@
+import { Button } from 'antd';
 import React, { JSX } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 0.5rem;
 `;
 
 const Title = styled.h1`
   font-size: 24px;
+  margin: 0;
 `;
 
 const Header = (): JSX.Element => (
   <Wrapper>
     <Title>Stream Bridge</Title>
+    <Button href='https://forms.gle/mhjAmBXH17218qnZA' target='_blank' danger>
+      Report a bug
+    </Button>
   </Wrapper>
 );
 
